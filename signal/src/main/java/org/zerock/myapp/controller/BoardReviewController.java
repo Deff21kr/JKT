9dfb6705c4a5401fbeaa98a66be8deac7235459f
Deff21kr <deff21kr@gmail.com>
2023-05-01 15:59:50 +0900

    no message

--- signal/src/main/java/org/zerock/myapp/controller/BoardReviewController.java
+++ signal/src/main/java/org/zerock/myapp/controller/BoardReviewController.java
@@ -11,5 +11,17 @@ import lombok.extern.log4j.Log4j2;
 //@RequestMapping("/users")
 @Controller
 public class BoardReviewController {
+/*
+ * 각 게시판은 CRUD 가 모두 가능
+ * 등록하기 위해 글을 쓰고
+ * 수정을 하고
+ * 특정 글을 조회하고
+ * 모든 글을 조회하고
+ * 삭제도 가능!
+ * FAQ는 하는사람이 알아서 생각바람
+ * 
+ * 추가적으로 페이지네이션과 댓글도
+ */
 
+	
 }

d8c55a8afbe434eae728f9baabd29c405a051fd0
Deff21kr <deff21kr@gmail.com>
2023-05-01 15:05:39 +0900

    컨트롤러

---
+++ signal/src/main/java/org/zerock/myapp/controller/Board1on1Controller.java
@@ -0,0 +1,15 @@
+package org.zerock.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@NoArgsConstructor
+@Log4j2
+
+//@RequestMapping("/users")
+@Controller
+public class Board1on1Controller {
+
+}

2b5f35a1cb524dd0aca8a418172fd457b3a5b101
LAPTOP-61V1RHRK\82108 <herrung@gmail.com>
2023-05-08 13:22:03 +0900

    no message

--- signal/src/test/java/org/zerock/myapp/mapper/JUnit4Template.java
+++ signal/src/test/java/org/zerock/myapp/mapper/JUnit4Template.java
@@ -5,8 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.NoArgsConstructor;

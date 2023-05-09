1f1986d50b5223aed63ef36d322d72bffa91c22b
Deff21kr <deff21kr@gmail.com>
2023-05-09 14:57:51 +0900

    비크립트 root에 빈으로 등록

--- signal/src/main/java/org/zerock/myapp/controller/UsersController.java
+++ signal/src/main/java/org/zerock/myapp/controller/UsersController.java
@@ -87,7 +87,7 @@ public class UsersController {
 		log.trace("get() 인보크");
 		
 		try {
-			UsersDTO vo = this.service.get(ID);
+			UsersVO vo = this.service.get(ID);
 			model.addAttribute("__BOARD__",vo);
 			
 //			return "뷰이름";

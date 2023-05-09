f20379842c6b518cf8a3baee5b923b21fdfc6b9e
Deff21kr <deff21kr@gmail.com>
2023-05-09 20:16:16 +0900

    FAQ 컨트롤러 맵핑 끝

--- signal/src/main/java/org/zerock/myapp/domain/FAQBoardDTO.java
+++ signal/src/main/java/org/zerock/myapp/domain/FAQBoardDTO.java
@@ -2,10 +2,10 @@ package org.zerock.myapp.domain;
 
 import java.sql.Timestamp;
 
-import lombok.Value;
+import lombok.Data;
 
-@Value
-public class FAQVO {
+@Data
+public class FAQBoardDTO {
 	private Integer postNo;
 	private Integer managerId;
 	private String title;

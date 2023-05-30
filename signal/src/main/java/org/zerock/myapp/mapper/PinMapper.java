a6dad44ad149922a8eb0eeb8eb6eecb8662df335
Deff21kr <deff21kr@gmail.com>
2023-05-30 18:48:01 +0900

    제발2

---
+++ signal/src/main/java/org/zerock/myapp/mapper/PinMapper.java
@@ -0,0 +1,24 @@
+package org.zerock.myapp.mapper;
+
+import java.util.List;
+
+import org.zerock.myapp.domain.PinDTO;
+
+public interface PinMapper {
+	
+	public abstract Integer insert(PinDTO pin) throws Exception;
+	
+	public abstract Integer delete(Integer pinNo);
+	
+//	찜 수정 
+//	public Integer modify(PinDTO pin);
+	
+//	찜 목록
+	public abstract List<PinDTO> selectList(String nickName);
+	
+	public abstract PinDTO select(Integer postNo, String nickName);
+	
+//	회원정보와 게시글 정보를 넘겨서 해당 row가 있는지 확인
+	public abstract Integer checkPin(PinDTO pin);
+	
+} // end interface
